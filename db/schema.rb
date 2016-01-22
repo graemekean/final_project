31300fbcb6f6c76deea90d1209daebd269041912
graemekean <graemekean@gmail.com>
2016-01-22 17:56:27 +0000

    added state_machine gem and added state column to the friendships table in the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122120007) do
+ActiveRecord::Schema.define(version: 20160122175302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 20160122120007) do
     t.integer  "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "state"
   end
 
+  add_index "friendships", ["state"], name: "index_friendships_on_state", using: :btree
   add_index "friendships", ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", using: :btree
 
   create_table "memberships", force: :cascade do |t|
